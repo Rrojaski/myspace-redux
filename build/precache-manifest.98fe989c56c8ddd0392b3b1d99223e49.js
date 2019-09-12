8a6b725500c4b2f9558a9730a08cee5ce9d1faa9
Roman Rojas <rrojaski@gmail.com>
2019-09-11 21:21:49 -0400

    added buid

---
+++ build/precache-manifest.98fe989c56c8ddd0392b3b1d99223e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedc193b057d06299f73c2a2629c0a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35a5db37a6ca4f4fb3be",
+    "url": "/static/css/main.693af4e7.chunk.css"
+  },
+  {
+    "revision": "115c5d2a8da0d93add2a",
+    "url": "/static/js/2.e8cb6220.chunk.js"
+  },
+  {
+    "revision": "35a5db37a6ca4f4fb3be",
+    "url": "/static/js/main.b1e6db83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2444a1eb3665ba1af39392b2f97ca16",
+    "url": "/static/media/apple.d2444a1e.png"
+  },
+  {
+    "revision": "469b691d0b16d09a795467dc1c4653f1",
+    "url": "/static/media/tom.469b691d.jpg"
+  }
+]);
